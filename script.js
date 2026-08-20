4cca90b93c86eee401e31e3a89fbf18b8a1b2557
Larissa Seixas <seixas.larissa@escola.pr.gov.br>
2026-08-20 12:01:20 -0300

    ok

--- script.js
+++ script.js
@@ -3,10 +3,21 @@
 document.addEventListener("DOMContentLoaded",() => {
     const likeBtn = document.querySelector(".left-actions .action-btn:fist-child");
     if (condition!likeBtn) return;
-    const likeSvg {
+    const likeSvg = likeBtn.querySelector("svg");{
     //localiza o contador
 
-    let textNode = Array.from(likeBtn.childNodes).find(node) =>
+    let textNode = Array.from(likeBtn.childNodes).find(node) => node.nodeType
+    === Node. TEXT_NODE && node.textContent.trim() !==""
 
     }
-} )
+} );
+
+//zera o contador
+let cont = 0;
+
+//atualiza
+if(textNode){
+    textNode.textContent = '0';
+}
+
+
